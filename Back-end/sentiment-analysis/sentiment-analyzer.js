1de907313d95800345530ca90d95ea37079ff25e
CsergezanIstvan <csergezan.istvan6@gmail.com>
2023-10-27 11:39:55 +0300

    Upgraded sentiment-analyzer

--- Back-end/sentiment-analysis/sentiment-analyzer.js
+++ Back-end/sentiment-analysis/sentiment-analyzer.js
@@ -18,10 +18,20 @@ export const analyzeSentiment = (text) => {
 
     for (let index in filteredText){
         const lemmaWord = lemmatizer(filteredText[index]);
+        
+        let modifier = 0;
+        if (index > 0 && words.enhencers.includes(filteredText[index-1])){
+            modifier++;
+        } else if (index > 0 && words.negators.includes(filteredText[index-1])){
+            modifier--;
+        }
+
         if (words.positives.includes(lemmaWord)){
             positiveWordCount++;
+            positiveWordCount += modifier;
         }else if (words.negatives.includes(lemmaWord)){
             negativeWordCount++;
+            negativeWordCount -= modifier;
         }
     }
 
